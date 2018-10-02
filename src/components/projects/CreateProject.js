bde392483dd1682ffad07098b40b800e206cf696
Stephanie <stephcshea@gmail.com>
2018-10-02 15:54:04 -0400

    create new project component working on console

---
+++ src/components/projects/CreateProject.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+class CreateProject extends Component {
+  state = {
+    title: '',
+    content: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  }
+  render() {
+    return (
+      <div className="container">
+        <form onSubmit={this.handleSubmit} className="white">
+          <h5 className="grey-text text-darken-3">
+            Create new project
+          </h5>
+          <div className="input-field">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" onChange={this.handleChange}/>
+          </div> 
+
+          <div className="input-field">
+          <label htmlFor="content">Project Content</label>
+           <textarea id="content" className="materialize-text"   onChange={this.handleChange}></textarea> 
+
+          </div> <button className="btn pink lighten-1 z-depth-0">Create</button>
+          <div className="input-field">
+
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default CreateProject
