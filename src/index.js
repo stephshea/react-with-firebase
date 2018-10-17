8b832b9a1136d15acd9fe979cf15fd15716f406d
Stephanie <stephcshea@gmail.com>
2018-10-17 19:59:58 -0400

    set up firestore

--- src/index.js
+++ src/index.js
@@ -13,10 +13,9 @@ import fbConfig from './config/fbConfig'
 
 const store = createStore(rootReducer,
   compose( 
-    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore }),
+    applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
     reduxFirestore(fbConfig),
     reactReduxFirebase(fbConfig)
-  )
   )
 );
 
